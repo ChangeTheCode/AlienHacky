5e33a8891ccb270c61f9a2b317d8f7981e8285c6
Aripas <tobias.breuss@students.fhv.at>
2016-10-03 09:53:31 +0200

    GyroTest wurde von Airmouse kopiert und einfache UART Ausgaben funktionieren

---
+++ Beispiele/airmouse/Debug/usb_dev_comp_hid.d
@@ -0,0 +1,53 @@
+# FIXED
+
+usb_dev_comp_hid.obj: ../usb_dev_comp_hid.c
+usb_dev_comp_hid.obj: C:/ti/ccsv6/tools/compiler/arm_5.2.8/include/stdint.h
+usb_dev_comp_hid.obj: C:/ti/ccsv6/tools/compiler/arm_5.2.8/include/stdbool.h
+usb_dev_comp_hid.obj: C:/ti/ccsv6/tools/compiler/arm_5.2.8/include/yvals.h
+usb_dev_comp_hid.obj: C:/ti/ccsv6/tools/compiler/arm_5.2.8/include/stdarg.h
+usb_dev_comp_hid.obj: C:/ti/ccsv6/tools/compiler/arm_5.2.8/include/linkage.h
+usb_dev_comp_hid.obj: C:/ti/ccsv6/tools/compiler/arm_5.2.8/include/_lock.h
+usb_dev_comp_hid.obj: C:/ti/TivaWare_C_Series-2.1.3.156/inc/hw_types.h
+usb_dev_comp_hid.obj: C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/rom_map.h
+usb_dev_comp_hid.obj: C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/rom.h
+usb_dev_comp_hid.obj: C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/sysctl.h
+usb_dev_comp_hid.obj: C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/gpio.h
+usb_dev_comp_hid.obj: C:/ti/TivaWare_C_Series-2.1.3.156/usblib/usblib.h
+usb_dev_comp_hid.obj: C:/ti/TivaWare_C_Series-2.1.3.156/usblib/usbhid.h
+usb_dev_comp_hid.obj: C:/ti/TivaWare_C_Series-2.1.3.156/usblib/device/usbdevice.h
+usb_dev_comp_hid.obj: C:/ti/TivaWare_C_Series-2.1.3.156/usblib/device/usbdevicepriv.h
+usb_dev_comp_hid.obj: C:/ti/TivaWare_C_Series-2.1.3.156/usblib/device/usbdcomp.h
+usb_dev_comp_hid.obj: C:/ti/TivaWare_C_Series-2.1.3.156/usblib/device/usbdhid.h
+usb_dev_comp_hid.obj: C:/ti/TivaWare_C_Series-2.1.3.156/usblib/device/usbdhidmouse.h
+usb_dev_comp_hid.obj: C:/ti/TivaWare_C_Series-2.1.3.156/usblib/device/usbdhidkeyb.h
+usb_dev_comp_hid.obj: C:/ti/TivaWare_C_Series-2.1.3.156/examples/boards/ek-tm4c123gxl-boostxl-senshub/drivers/buttons.h
+usb_dev_comp_hid.obj: C:/ti/TivaWare_C_Series-2.1.3.156/examples/boards/ek-tm4c123gxl-boostxl-senshub/drivers/rgb.h
+usb_dev_comp_hid.obj: ../usb_structs.h
+usb_dev_comp_hid.obj: ../events.h
+usb_dev_comp_hid.obj: ../motion.h
+
+../usb_dev_comp_hid.c: 
+C:/ti/ccsv6/tools/compiler/arm_5.2.8/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/arm_5.2.8/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/arm_5.2.8/include/yvals.h: 
+C:/ti/ccsv6/tools/compiler/arm_5.2.8/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/arm_5.2.8/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/arm_5.2.8/include/_lock.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/rom_map.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/rom.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/usblib/usblib.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/usblib/usbhid.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/usblib/device/usbdevice.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/usblib/device/usbdevicepriv.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/usblib/device/usbdcomp.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/usblib/device/usbdhid.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/usblib/device/usbdhidmouse.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/usblib/device/usbdhidkeyb.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/examples/boards/ek-tm4c123gxl-boostxl-senshub/drivers/buttons.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/examples/boards/ek-tm4c123gxl-boostxl-senshub/drivers/rgb.h: 
+../usb_structs.h: 
+../events.h: 
+../motion.h: 
