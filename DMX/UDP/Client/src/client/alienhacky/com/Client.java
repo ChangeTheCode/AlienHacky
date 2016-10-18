fa4c0d03a4adaa497775f6bc37ce36b447dabc7e
Ursus Schneider <develop@ursus.cc>
2016-10-18 08:44:11 +0200

    OpenDMX Test (funktionierte leider nicht, DLL in Verwendung (geht nicht am Mac)), UDP Test (senden und empfangen funktioniert), Erste versuche DMX Protokoll (geht nicht), ESP Konfiguration ausgelesen, Zeitblätter

--- DMX/UDP/Client/src/client/alienhacky/com/Client.java
+++ DMX/UDP/Client/src/client/alienhacky/com/Client.java
@@ -3,33 +3,36 @@ package client.alienhacky.com;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 
 public class Client {
+	
+	public static final String ESP_IP = "10.0.0.200";
+	public static final int ESP_PORT = 3330;
+
 
 	public static void main(String[] args) {
 
 		try {
-			String host = "127.0.0.1";
-			int port = 9999;
+			
+			System.out.println ("AlienServer UDP Send commands Version 17.10.2016");
+			System.out.println ("Sending to IP: " + ESP_IP);
+			System.out.println ("Sending on port: " + ESP_PORT);
 
-			byte[] message = "Hello there - are you home?".getBytes();
+			byte[] message = "ESDD001".getBytes();
 			// ByteBuffer buffer;
 			// buffer = ByteBuffer.wrap(message);
 			// buffer.order(ByteOrder.BIG_ENDIAN);
 
 			// Get the internet address of the specified host
-			InetAddress address = InetAddress.getByName(host);
+			InetAddress address = InetAddress.getByName (ESP_IP);
 
 			// Initialize a datagram packet with data and address
-			DatagramPacket packet = new DatagramPacket (message, message.length,
-					address, port);
+			DatagramPacket packet = new DatagramPacket (message, message.length, address, ESP_PORT);
 
 			// Create a datagram socket, send the packet through it, close it.
 			DatagramSocket dsocket = new DatagramSocket();
+			// dsocket.setBroadcast(true);
 			dsocket.send(packet);
-			dsocket.setBroadcast(true);
 			
 			dsocket.close();
 		} catch (Exception e) {
--- DMX/UDP/Server/src/server/alienhacky/com/Server.java
+++ DMX/UDP/Server/src/server/alienhacky/com/Server.java
@@ -2,18 +2,26 @@ package server.alienhacky.com;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
 
 public class Server {
+	
+	public static final String MY_IP = "10.0.0.1";
+	public static final int MY_PORT = 3333;
+	
 	public static void main(String[] args) {
 		try {
-			int port = 9999;
 			
-			System.out.println("Running on port: " + port);
+			System.out.println ("AlienServer UDP Listing Version 17.10.2016");
+			System.out.println ("Listening to IP: " + MY_IP);
+			System.out.println ("Listening on port: " + MY_PORT);
 
 			// Create a socket to listen on the port.
 			@SuppressWarnings("resource")
-			DatagramSocket dsocket = new DatagramSocket(port);
-
+			DatagramSocket dsocket = new DatagramSocket(null);
+			InetSocketAddress address = new InetSocketAddress(MY_IP, MY_PORT);
+			dsocket.bind(address);
+			
 			// Create a buffer to read datagrams into. If a
 			// packet is larger than this buffer, the
 			// excess will simply be discarded!
