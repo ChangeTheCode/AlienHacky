f625f1625cc34c70ea9eba6299530566158fcb4a
Aripas <tobias.breuss@students.fhv.at>
2016-10-29 16:38:17 +0200

    Kommunikation von 2 Boards möglich; mac ausgelesen; noch keine Antwort wird versendet

--- Beispiele/rfPacketTx_CC2650_LAUNCHXL_TI/rfPacketTx.c
+++ Beispiele/rfPacketTx_CC2650_LAUNCHXL_TI/rfPacketTx.c
@@ -42,22 +42,42 @@
 #include <ti/drivers/rf/RF.h>
 #include <ti/drivers/PIN.h>
 
+#include <ti/drivers/UART.h>
+
+#include <inc/hw_fcfg1.h>
+
 /* Board Header files */
 #include "Board.h"
 
 #include "smartrf_settings/smartrf_settings.h"
 
-/* Pin driver handle */
+/* Pin driver handles */
+static PIN_Handle buttonPinHandle;
 static PIN_Handle ledPinHandle;
+
+/* Global memory storage for a PIN_Config table */
+static PIN_State buttonPinState;
 static PIN_State ledPinState;
 
+
 /*
- * Application LED pin configuration table:
- *   - All LEDs board LEDs are off.
+ * Initial LED pin configuration table
+ *   - LEDs Board_LED0 is on.
+ *   - LEDs Board_LED1 is off.
  */
-PIN_Config pinTable[] =
-{
-    Board_LED1 | PIN_GPIO_OUTPUT_EN | PIN_GPIO_LOW | PIN_PUSHPULL | PIN_DRVSTR_MAX,
+PIN_Config ledPinTable[] = {
+    Board_LED0 | PIN_GPIO_OUTPUT_EN | PIN_GPIO_HIGH | PIN_PUSHPULL | PIN_DRVSTR_MAX,
+    Board_LED1 | PIN_GPIO_OUTPUT_EN | PIN_GPIO_LOW  | PIN_PUSHPULL | PIN_DRVSTR_MAX,
+    PIN_TERMINATE
+};
+
+/*
+ * Application button pin configuration table:
+ *   - Buttons interrupts are configured to trigger on falling edge.
+ */
+PIN_Config buttonPinTable[] = {
+    Board_BUTTON0  | PIN_INPUT_EN | PIN_PULLUP | PIN_IRQ_NEGEDGE,
+    Board_BUTTON1  | PIN_INPUT_EN | PIN_PULLUP | PIN_IRQ_NEGEDGE,
     PIN_TERMINATE
 };
 
@@ -67,8 +87,8 @@ PIN_Config pinTable[] =
 #define TX_TASK_PRIORITY   2
 
 /* Packet TX Configuration */
-#define PAYLOAD_LENGTH      30
-#define PACKET_INTERVAL     (uint32_t)(4000000*0.5f) /* Set packet interval to 500ms */
+#define PAYLOAD_LENGTH      19
+//#define PACKET_INTERVAL     (uint32_t)(16000000*0.5f) /* Set packet interval to 500ms */
 
 
 
@@ -86,9 +106,16 @@ static RF_Object rfObject;
 static RF_Handle rfHandle;
 
 uint32_t time;
-static uint8_t packet[PAYLOAD_LENGTH];
+//static uint8_t packet[PAYLOAD_LENGTH];
+uint8_t payload[] = "HelloBigNewWorld\n";
+uint8_t packet[PAYLOAD_LENGTH];
 static uint16_t seqNumber;
-static PIN_Handle pinHandle;
+
+PIN_Handle pinHandle;
+
+uint8_t button_pressed = 0;
+
+uint8_t uart_text[] = "packet gesendet\n";
 
 
 /***** Function definitions *****/
@@ -107,13 +134,36 @@ void TxTask_init(PIN_Handle inPinHandle)
 
 static void txTaskFunction(UArg arg0, UArg arg1)
 {
-    uint32_t time;
+	// uart init
+	UART_Handle uart;
+	UART_Params uartParams;
+
+	const char echoPrompt[] = "\fEchoing characters:\r\n";
+
+	/* Create a UART with data processing off. */
+	UART_Params_init(&uartParams);
+	uartParams.writeDataMode = UART_DATA_BINARY;
+	uartParams.readDataMode = UART_DATA_BINARY;
+	uartParams.readReturnMode = UART_RETURN_FULL;
+	uartParams.readEcho = UART_ECHO_OFF;
+	uartParams.baudRate = 9600;
+	uart = UART_open(Board_UART0, &uartParams);
+
+	if (uart == NULL) {
+		System_abort("Error opening the UART");
+	}
+
+	UART_write(uart, echoPrompt, sizeof(echoPrompt));
+
+	// rf init
     RF_Params rfParams;
     RF_Params_init(&rfParams);
 
+   // RF_cmdPropTx.pktLen = PAYLOAD_LENGTH;
     RF_cmdPropTx.pktLen = PAYLOAD_LENGTH;
     RF_cmdPropTx.pPkt = packet;
-    RF_cmdPropTx.startTrigger.triggerType = TRIG_ABSTIME;
+    //RF_cmdPropTx.startTrigger.triggerType = TRIG_ABSTIME;
+    RF_cmdPropTx.startTrigger.triggerType = TRIG_NOW;
     RF_cmdPropTx.startTrigger.pastTrig = 1;
     RF_cmdPropTx.startTime = 0;
 
@@ -123,32 +173,101 @@ static void txTaskFunction(UArg arg0, UArg arg1)
     /* Set the frequency */
     RF_postCmd(rfHandle, (RF_Op*)&RF_cmdFs, RF_PriorityNormal, NULL, 0);
 
-    /* Get current time */
-    time = RF_getCurrentTime();
+
+//    /* Get current time */
+//    time = RF_getCurrentTime();
+//    while(1)
+//    {
+//        /* Create packet with incrementing sequence number and random payload */
+//        packet[0] = (uint8_t)(seqNumber >> 8);
+//        packet[1] = (uint8_t)(seqNumber++);
+//        uint8_t i;
+//        for (i = 2; i < PAYLOAD_LENGTH; i++)
+//        {
+//            //packet[i] = rand();
+//        	packet[i] = payload[i-2];
+//        }
+//
+//        /* Set absolute TX time to utilize automatic power management */
+//        time += PACKET_INTERVAL;
+//        RF_cmdPropTx.startTime = time;
+//
+//        /* Send packet */
+//        RF_EventMask result = RF_runCmd(rfHandle, (RF_Op*)&RF_cmdPropTx, RF_PriorityNormal, NULL, 0);
+//        if (!(result & RF_EventLastCmdDone))
+//        {
+//            /* Error */
+//            while(1);
+//        }
+//
+//        PIN_setOutputValue(pinHandle, Board_LED1,!PIN_getOutputValue(Board_LED1));
+//    }
+
+	uint64_t macAddress = *((uint64_t *)(FCFG1_BASE + FCFG1_O_MAC_15_4_0)) & 0xFFFFFFFFFFFF;
+	uint8_t a[8];
+	memcpy(a, &macAddress, 8);  //TODO: mac addresse noch falsch rum
+
     while(1)
     {
-        /* Create packet with incrementing sequence number and random payload */
-        packet[0] = (uint8_t)(seqNumber >> 8);
-        packet[1] = (uint8_t)(seqNumber++);
-        uint8_t i;
-        for (i = 2; i < PAYLOAD_LENGTH; i++)
-        {
-            packet[i] = rand();
+    	if(button_pressed == 1)
+    	{
+			button_pressed = 0;
+
+			/* Create packet with incrementing sequence number and random payload */
+			packet[0] = (uint8_t)(seqNumber >> 8);
+			packet[1] = (uint8_t)(seqNumber++);
+
+			uint8_t i;
+			for (i = 2; i < PAYLOAD_LENGTH-6; i++)
+			{
+				//packet[i] = rand();
+				packet[i] = payload[i-2];
+			}
+
+			uint8_t j;
+			for (j = 0; j < 6; j++)
+			{
+				packet[j+i+1] = a[j];   //TODO: Mac addresse noch falschrum
+			}
+
+
+			/* Send packet */
+			RF_EventMask result = RF_runCmd(rfHandle, (RF_Op*)&RF_cmdPropTx, RF_PriorityNormal, NULL, 0);
+			if (!(result & RF_EventLastCmdDone))
+			{
+				/* Error */
+				while(1);
+			}
+			UART_write(uart, &uart_text, 16);
+
+    	}
+    }
+}
+
+void buttonCallbackFxn(PIN_Handle handle, PIN_Id pinId) {
+    uint32_t currVal = 0;
+
+    /* Debounce logic, only toggle if the button is still pushed (low) */
+    CPUdelay(8000*50);
+    if (!PIN_getInputValue(pinId)) {
+        /* Toggle LED based on the button pressed */
+        switch (pinId) {
+            case Board_BUTTON0:
+                currVal =  PIN_getOutputValue(Board_LED0);
+                PIN_setOutputValue(ledPinHandle, Board_LED0, !currVal);
+                button_pressed = 1;
+
+                break;
+
+            case Board_BUTTON1:
+                currVal =  PIN_getOutputValue(Board_LED1);
+                PIN_setOutputValue(ledPinHandle, Board_LED1, !currVal);
+                break;
+
+            default:
+                /* Do nothing */
+                break;
         }
-
-        /* Set absolute TX time to utilize automatic power management */
-        time += PACKET_INTERVAL;
-        RF_cmdPropTx.startTime = time;
-
-        /* Send packet */
-        RF_EventMask result = RF_runCmd(rfHandle, (RF_Op*)&RF_cmdPropTx, RF_PriorityNormal, NULL, 0);
-        if (!(result & RF_EventLastCmdDone))
-        {
-            /* Error */
-            while(1);
-        }
-
-        PIN_setOutputValue(pinHandle, Board_LED1,!PIN_getOutputValue(Board_LED1));
     }
 }
 
@@ -157,15 +276,30 @@ static void txTaskFunction(UArg arg0, UArg arg1)
  */
 int main(void)
 {
+
+
     /* Call board init functions. */
     Board_initGeneral();
+    Board_initUART();
+
+
 
     /* Open LED pins */
-    ledPinHandle = PIN_open(&ledPinState, pinTable);
+    ledPinHandle = PIN_open(&ledPinState, ledPinTable);
     if(!ledPinHandle)
     {
         System_abort("Error initializing board LED pins\n");
     }
+
+    buttonPinHandle = PIN_open(&buttonPinState, buttonPinTable);
+	if(!buttonPinHandle) {
+		System_abort("Error initializing button pins\n");
+	}
+
+	/* Setup callback for button pins */
+	if (PIN_registerIntCb(buttonPinHandle, &buttonCallbackFxn) != 0) {
+		System_abort("Error registering button callback function");
+	}
 
     /* Initialize task */
     TxTask_init(ledPinHandle);
