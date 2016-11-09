d25e1d5ecb0456ba807a23a6e9a3a3d97532a5c8
Jim <jim.frey@students.fhv.at>
2016-11-09 10:34:23 +0100

    gyro header

---
+++ Beispiele/i2c_TI_example/Sensor_lib/mpu9150.c
@@ -0,0 +1,15 @@
+/*
+ * mpu9150.c
+ *
+ *  Created on: 09.11.2016
+ *      Author: Jim
+ */
+
+#include "../AlienHacky_datatypes.h"
+
+BOOL mpu9150_write(int reg_address, int value);
+
+ mpu9150_read(int reg_address, int count_of_bytes);
+
+
+
