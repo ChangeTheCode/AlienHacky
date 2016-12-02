227722bdff21085f55ab66c2b56c3e0cf7b37372
Jim <jim.frey@students.fhv.at>
2016-12-02 20:18:18 +0100

    Node in den Lichtbranche geholt

---
+++ Projektcode/Node_Launchpad/pin.h
@@ -0,0 +1,14 @@
+/*
+ * pin.h
+ *
+ *  Created on: 22. Nov. 2016
+ *      Author: Tobias
+ */
+
+#ifndef PIN_H_
+#define PIN_H_
+
+void LED_init(void);
+void button_init(void);
+
+#endif /* PIN_H_ */
