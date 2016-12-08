70d79e55db57ddb68901c7901a90a9db435d29fe
EvilDre4m <tri7484@students.fhv.at>
2016-12-08 15:28:44 +0100

    MHControl Test fertig
    Funktioniert nach kleinen fixes, versch. Tests gemacht

--- AlienServer/AlienServerProject/src/at/fhv/alienserver/movingHead/MHControl.java
+++ AlienServer/AlienServerProject/src/at/fhv/alienserver/movingHead/MHControl.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.concurrent.BlockingQueue;
 
+import static java.lang.Math.abs;
 import static java.lang.Thread.sleep;
 
 /**
@@ -16,18 +17,13 @@ import static java.lang.Thread.sleep;
  * @version 1.11.2016
  */
 public class MHControl {
-    private BlockingQueue<Tuple<CoordinateContainer, Long>> coordinatesFromCalculator;
-    /**
-     * Integer that determines for what queue size the MH - Control will wait to be available. This directly sets which
-     * number of coordinates is actually set with the MH - X25 (e.g. every 20th position)
-     */
     private ESP esp = new ESP();
 
     //FIXME: Move all of the following to config.file
     /**
      * Offset of the Pan - angle; chosen to have the MH-X25 point straight to the ground for point (0, 0)
      */
-    private final double offset_pan = 270;
+    private final double offset_pan = 90;
     //private final double offset_pan = 180;
     /**
      * Offset of the Tilt - angle; chosen to have the MH-X25 point straight to the ground for point (0, 0)
@@ -37,7 +33,7 @@ public class MHControl {
     /**
      * Height of the moving head's mounting point in meters; measured at the tilt-turning-axis
      */
-    private final double h = 2.4;
+    private final double h = 2.57;
     /**
      * Offset of the moving head relative to center of game coordinate system in x - direction
      */
@@ -54,7 +50,15 @@ public class MHControl {
      */
     private CoordinateContainer oldPosition;
 
+    /**
+     * A DMX used to store the last used packet. Needed to determine the old angles if the caller wishes for self
+     * triggered sleeping.
+     */
+    private DMX oldPacket;
+
     private double exaggerationFactor;
+    private int xMirrorFactor;
+    private int yMirrorFactor;
 
     public double getExaggerationFactor() {
         return exaggerationFactor;
@@ -64,16 +68,33 @@ public class MHControl {
         this.exaggerationFactor = exaggerationFactor;
     }
 
-    public MHControl(double exaggerationFactor) throws IOException {
+    public MHControl(double exaggerationFactor, boolean mirrorX, boolean mirrorY) throws IOException {
         this.exaggerationFactor = exaggerationFactor;
+        if(mirrorX){
+            xMirrorFactor = -1;
+        } else {
+            xMirrorFactor = 1;
+        }
+
+        if(mirrorY){
+            yMirrorFactor = -1;
+        } else {
+            yMirrorFactor = 1;
+        }
+
         DMX packet = new DMX();
         //The following three lines set the MH-X25 to position (0,0)
         packet.setPan(offset_pan);
         packet.setTilt(offset_tilt);
+
+        oldPacket = new DMX(packet);
+        oldPosition = new CoordinateContainer(0,0);
+
         esp.sendPackets(packet);
     }
 
-    public void setPosition(CoordinateContainer position, boolean exaggerate){
+    public void setPosition(CoordinateContainer position, boolean exaggerate, boolean selfSleep){
+        long sleepTime = 0;
         LinkedList<DMX> packets = new LinkedList<>();
 
         DMX dmxPacket = new DMX();
@@ -82,26 +103,37 @@ public class MHControl {
         position.x = position.x + mhOffsetX;
         position.y = position.y + mhOffsetY;
 
+        position.x *= xMirrorFactor;
+        position.y *= yMirrorFactor;
+
         dmxPacket.setPan(Math.atan(position.x / h) * 180 / Math.PI + offset_pan);
         dmxPacket.setTilt(Math.atan(position.y / Math.sqrt(position.x * position.x + h * h)) * 180 / Math.PI + offset_tilt);
+
+        if(selfSleep){
+            sleepTime = getTimeToSleep(oldPacket, dmxPacket);
+        }
 
         packets.add(dmxPacket);
 
         if(exaggerate) {
             exaggeratedDmxPacket = getExaggeratedPacket(position, oldPosition, this.exaggerationFactor);
-            //Change lighting to make use of exaggerated packet visible
+
+            //Change lighting to make use of exaggerated packet visible; adds no functionality, just visualisation
             exaggeratedDmxPacket.shutter = (byte) 0;
             exaggeratedDmxPacket.dimmer = (byte) 0;
+
             packets.addFirst(exaggeratedDmxPacket);
         }
 
         oldPosition = new CoordinateContainer(position);
+        oldPacket = new DMX(dmxPacket);
 
         try {
             for (DMX packet : packets) {
                 esp.sendPackets(packet);
-                sleep(20);
+                sleep(150); //FIXME: this sleep call is also used when no exaggerated packet is used
             }
+            sleep(sleepTime);
         } catch (IOException e) {
             System.err.println("Couldn't transmit ESP-packet for shit!");
             System.err.println(e.toString());
@@ -109,7 +141,6 @@ public class MHControl {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
     }
 
     private DMX getExaggeratedPacket(CoordinateContainer pos, CoordinateContainer oldPos, double factor){
@@ -129,6 +160,43 @@ public class MHControl {
         packet.setTilt(Math.atan(tempC.y / Math.sqrt(tempC.x * tempC.x + h * h)) * 180 / Math.PI + offset_tilt);
 
         return packet;
+    }
+
+    /**
+     * Function to calculate the time for which the control sleeps to allow the MH-X25 completing its motion
+     * <p>
+     * With its two parameters the function determines how far the MH-X25 has to move and sets up the returned time
+     * accordingly. More precisely it looks at the changes of pan and tilt angles, determines which one is greater in
+     * absolute numbers. It then takes the greater value, calculates the natural logarithm of it and multiplies it
+     * with an empirically determined factor.
+     * <p>
+     * The use of a logarithm was chosen to give small changes a bigger weight compared to bigger changes. The reason
+     * behind this is the fact that the MH-X25 moves faster and accelerates harder when it has a bigger distance to
+     * travel. Hence we need proportionally more waiting time for smaller changes.
+     *
+     * @param oldPacket The packet used to set the MH-X25's position before the currently used one.
+     * @param newPacket The packet currently in use to set the MH-X25's position
+     * @return Waiting time in milliseconds in "long" data format.
+     */
+    private long getTimeToSleep(DMX oldPacket, DMX newPacket){
+        int deltaPan = newPacket.pan - oldPacket.pan;
+        int deltaTilt = newPacket.tilt - oldPacket.tilt;
+        int bigger = (abs(deltaPan) > abs(deltaTilt))? abs(deltaPan) : abs(deltaTilt);
+        bigger = (bigger < 1)? bigger + 1 : bigger;
+        System.out.println("Bigger = " + bigger);
+        return (long)(log2(bigger) * 350);
+    }
+
+    private int log2(int bits){
+        /*
+        Created with: http://stackoverflow.com/questions/3305059/how-do-you-calculate-log-base-2-in-java-for-integers#3305710
+         */
+        int log = 0;
+        if( ( bits & 0xffff0000 ) != 0 ) { bits >>>= 16; log = 16; }
+        if( bits >= 256 ) { bits >>>= 8; log += 8; }
+        if( bits >= 16  ) { bits >>>= 4; log += 4; }
+        if( bits >= 4   ) { bits >>>= 2; log += 2; }
+        return log + ( bits >>> 1 );
     }
 
 }
