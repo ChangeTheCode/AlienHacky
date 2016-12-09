23c31dd6ae2ebbb7625eeb8f0df671d31b33ae0e
Jim <jim.frey@students.fhv.at>
2016-12-09 14:17:26 +0100

    logic entfernt. da das erkennbar wird, wenn Null von der Berechner Lister zurückgegeben wird

--- AlienServer/AlienServerProject/src/at/fhv/alienserver/game/board.java
+++ AlienServer/AlienServerProject/src/at/fhv/alienserver/game/board.java
@@ -19,31 +19,30 @@ public class board implements Runnable{
 
     private static Logic _game_rules;
 
-    private CoordinateContainer _start_point =null;
+    private CoordinateContainer _start_point = null;
 
-    private CoordinateContainer _speed_kick_value;
+    private CoordinateContainer _next_kick_value;
+
+    public boolean get_is_kick_speed_is_new() {
+        return _kick_speed_is_new;
+    }
+
+    public void set_kick_speed_is_new(boolean _kick_speed_is_new) {
+        this._kick_speed_is_new = _kick_speed_is_new;
+    }
+
     private boolean _kick_speed_is_new = false;
 
     private static board ourInstance = new board();
 
-    public static board getInstance() {
-        ourInstance._start_point = new CoordinateContainer(0.0,0.0, "start_position");
-        ourInstance._speed_kick_value = new CoordinateContainer(-9999,-9999,"failed");
 
-        ourInstance._start_point = new CoordinateContainer(0.0,0.0, "start_position");
-
-        ourInstance._game_rules = null; // we can't config the rules without information about the gameboard
-
-        return ourInstance;
-    }
-
-    public static board getInstance(double zero_point_x, double zero_point_y , double left, double right, double top, double bottom) {
-        ourInstance._start_point = new CoordinateContainer(zero_point_x, zero_point_y);
+    public static board getInstance(double start_point_x, double start_point_y , double left, double right, double top, double bottom) {
+        ourInstance._start_point = new CoordinateContainer(start_point_x, start_point_y);
         ourInstance._bottom_border = bottom;
         ourInstance._top_border = top;
         ourInstance._right_border = right;
         ourInstance._left_border = left;
-        ourInstance._speed_kick_value = new CoordinateContainer(-9999,-9999,"failed");
+        ourInstance._next_kick_value = null;
 
         ourInstance._game_rules = Logic.getInstance();
         return ourInstance;
@@ -70,20 +69,19 @@ public class board implements Runnable{
     //diese funktion soll vom Server aufgerufen werden.
     public void set_speed_kick_value (Kick_Container new_kick, long start_time){
         CoordinateContainer coor_kick = new_kick.getKick_direction_speed();
-        if(this._speed_kick_value != null){
-            _speed_kick_value.x = coor_kick.x;
-            _speed_kick_value.y = coor_kick.y;
-        }else {
-            this._speed_kick_value = new CoordinateContainer(coor_kick.x, coor_kick.y);
+        if(this._next_kick_value != null) {
+            _next_kick_value = null;
         }
+        this._next_kick_value = new CoordinateContainer(coor_kick.x, coor_kick.y);
+
         _kick_speed_is_new = true;
     }
 
-    public CoordinateContainer get_speed_kick_value(){
-        if(_speed_kick_value != null) {
-            return this._speed_kick_value;
+    public CoordinateContainer get_next_kick_value(){
+        if(_next_kick_value != null) {
+            return this._next_kick_value;
         }
-        return new CoordinateContainer(-9999,-9999,"failed");
+        return null;
     }
 
 
@@ -96,8 +94,9 @@ public class board implements Runnable{
     }
 
     public void run(){
-        CoordinateContainer _speed_kick = new CoordinateContainer(_speed_kick_value);
+        CoordinateContainer _speed_kick = new CoordinateContainer(_next_kick_value);
         long last_move_time = 0;
+        boolean out_of_range = false;
 
         while(true){
 
@@ -108,20 +107,64 @@ public class board implements Runnable{
             while (last_move_time + 10 <= System.currentTimeMillis() || ! _kick_speed_is_new ){
                 // check if there is a new kick event
                 if(_kick_speed_is_new){
-                    _speed_kick = new CoordinateContainer(_speed_kick_value);
-                    this._speed_kick_value.x = -9999;
-                    this._speed_kick_value.x = -9999;
+                    _next_kick_value = null;
 
                     _kick_speed_is_new =false;
-                    //Todo call calculate points                            //means startpoint
-                    // calculate( _speed_kick.start_time, _speed_kick.x, _speed_kick,y,_speed_kick,z, last_move_coordinate, geschwindigkeit? );
+                    //Todo call calculate points
+                    CoordinateContainer next_pos = null; // calculate( _speed_kick.start_time, _speed_kick.x, _speed_kick,y,_speed_kick,z, last_move_coordinate, geschwindigkeit? );
+                    if(next_pos == null){
+                        out_of_range = true;
+                        break;
+                    }
+
                     break; // jump out of the loop and start to get mh_control a new coordinate
                 }
 
                 // check, if you are still inside of the game board
+                if(out_of_range){
+                    out_of_range = false;
+
+                    do_game_over();
+                    do_start_game(true);
+                }
 
             }
 
+        }
+    }
+
+
+    void do_game_over(){
+        // move_to(start_point, purple)
+        long sys_time = System.currentTimeMillis();
+        int repeat = 0;
+        while (repeat < 3 ){
+            if((sys_time + 1000 ) >= System.currentTimeMillis()){
+                //set_intensive_of_light(0);
+                repeat ++;
+                sys_time = System.currentTimeMillis();
+            }
+        }
+
+    }
+
+    void do_start_game(boolean first_call){
+        long sys_time = System.currentTimeMillis();
+        int light_intensively  = 0;
+        if(first_call) {
+            System.out.println(" Started new game ");
+            first_call = false;
+            //move_to(start_point, red);
+        }
+        while(true) {
+            if ((sys_time + 1000) >= System.currentTimeMillis()) {
+                light_intensively ^= light_intensively;
+                //set_intensive_of_light( light_intensively *100);
+                sys_time = System.currentTimeMillis();
+            }
+            if (get_is_kick_speed_is_new()){
+                return;
+            }
         }
     }
 
