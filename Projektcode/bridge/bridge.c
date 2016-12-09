4ad420aa3161feab0598de5d16b16ce75fe5666f
Ursus Schneider <develop@ursus.cc>
2016-12-09 13:23:23 +0100

    Bridge (UART Teil) eingepflegt

---
+++ Projektcode/bridge/bridge.c
@@ -0,0 +1,34 @@
+/*
+ * Bridge.c
+ *
+ */
+#include "AlienUART.h"
+#include "RF.h"
+#include "button.h"
+#include "Board.h"
+#include <ti/sysbios/BIOS.h>
+#include <xdc/runtime/System.h>  // This is needed as Board_initGeneral () uses System_abort but didn't include it?
+
+BOOLEAN debug;
+
+int main(void) {
+
+	// are we displaying DEBUG messages
+	debug = TRUE;
+
+    // Start message
+	Alien_log ("AlienBridge starting. Running version 09.12.2016\n\n");
+
+    // Initialise the Board
+    Board_initGeneral();
+
+    // Initialise the UART
+    Alien_UART_init ();
+    Alien_RF_init ();
+    // button_init (SEND_TEST);
+    // button_init (RECEIVE_TEST);
+
+    // Now start BIOS
+    BIOS_start();
+    return (0);
+}
